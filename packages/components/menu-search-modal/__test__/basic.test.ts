d62b696d2a4d86b3d295dc555a8a5f5ddb9a0021
yuexing <yuexing@ideamake.cn>
2022-07-08 18:06:37 +0800

    feat(test): add test script

---
+++ packages/components/menu-search-modal/__test__/basic.test.ts
@@ -0,0 +1,19 @@
+import { test, expect, describe } from 'vitest'
+import { mount } from '@vue/test-utils'
+import MenuSearchModal from '../index'
+import Modal from 'ant-design-vue/es/modal'
+import Button from 'ant-design-vue/es/button'
+
+
+describe('MenuSearchModal', () => {
+  test('mount component', async () => {
+    expect(MenuSearchModal).toBeTruthy()
+    const wrapper = mount(MenuSearchModal, {
+      props: {
+        options: [],
+      },
+    })
+    expect(wrapper.findComponent(Modal).exists()).toBeTruthy()
+    expect(wrapper.findComponent(Button).exists()).toBeTruthy()
+  })
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vite'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vue from '@vitejs/plugin-vue'
@@ -11,6 +13,18 @@ export default defineConfig(() => {
       alias: {
         // 用来标记别名路径引入
         '@': resolve(__dirname, 'src'),
+      },
+    },
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      transformMode: {
+        web: [/.[tj]sx$/],
+      },
+      deps: {
+        inline: [
+          'ant-design-vue',
+        ],
       },
     },
   }
